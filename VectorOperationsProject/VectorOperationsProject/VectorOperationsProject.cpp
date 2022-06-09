6696e8dfc37f754b06e8ed3d888c0b1e3360d189
RAStoychev18 <RAStoychev18@codingburgas.bg>
2022-06-09 09:51:07 +0300

    Add Elements to a Vector

--- VectorOperationsProject/VectorOperationsProject/VectorOperationsProject.cpp
+++ VectorOperationsProject/VectorOperationsProject/VectorOperationsProject.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <vector>
+
+
 using namespace std;
+
+
 
 int main()
 {
-	cout << "Hello World!";
+	//Add Elements to a Vector
+
+
+	vector<int> num{ 4, 5, 8 };
+
+
+
+	num.push_back(1);
+	num.push_back(3);
+
+
+
+	for (const int& i : num) {
+		cout << i << " ";
+	}
 }
